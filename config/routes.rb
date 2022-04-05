9806811b1004b64572b3065d1e778aead6a29783
qadeer0020 <qadeer@acklib.com>
2022-04-05 15:32:12 +0500

    new commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/landing_page'
+  get 'static_pages/privacy_policy'
+  root "static_pages#privacy_policy"
+  get "landing_page", to:'static_pages#landing_page'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
